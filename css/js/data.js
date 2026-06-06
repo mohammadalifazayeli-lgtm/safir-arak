8122374d8ff46d34155e4087a5f6ad53783a2c12
mohammadalifazayeli-lgtm <mohammadalifazayeli@gmail.com>
2026-06-06 21:10:47 +0330

    Create data.js

---
+++ css/js/data.js
@@ -0,0 +1,20 @@
+export const courses = [
+  {
+    id: 1,
+    title: "English Basic",
+    level: "A1",
+    desc: "شروع زبان از صفر"
+  },
+  {
+    id: 2,
+    title: "Daily English",
+    level: "A2",
+    desc: "مکالمه روزمره"
+  },
+  {
+    id: 3,
+    title: "Conversation",
+    level: "B1",
+    desc: "مکالمه پیشرفته"
+  }
+];
